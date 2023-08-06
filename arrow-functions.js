fa49470121df0023cb6d9c286f01e63fec0a4661
Humberto Pereira <humberto_pereira@live.com>
2023-08-06 17:18:10 +0000

    commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -3,15 +3,15 @@
  * command node arrow-functions.js in the terminal
  */
 
-// Vanilla JavaScript Function
+//Vanilla JavaScript Function
 
-// function addTwoNumbers(a, b) {
-//     // code block
-//     return a + b;
-// }
+function addTwoNumbers(a, b) {
+    // code block
+    return a + b;
+}
 
-// let sum = addTwoNumbers(3, 5)
-// console.log(sum)
+let sum = addTwoNumbers(3, 5)
+console.log(sum)
 
 // -----------------------------------Arrow Function With Parameters
 
@@ -20,7 +20,7 @@ const addTwoNumbers = (a, b) => {
     return a + b;
 }
 
-let sum = addTwoNumbers(3, 5);
+let sum1 = addTwoNumbers(3, 5);
 console.log(sum);
 
 // Single Line Arrow Function With Parameters
@@ -103,3 +103,66 @@ const sumRest3 = (a, b, c, ...rest) => {
 let sumRest3Result = sumRest3(1, 2, 3, 4, 5, 6);
 console.log('sumRest3', sumRest3Result);
 
+// ------------------------------------REST PARAMETER SYNTAX
+
+// Destructuring arrays
+// Vanilla Js
+let ages = [30, 26, 27];
+// let john = ages[0];
+// let mary = ages[1];
+// let joe = ages[2];
+// console.log(john, mary, joe);
+
+// ES6 way
+let [john, mary, joe] = ages;
+console.log(john, mary, joe);
+
+// Destructuring objects
+let jobs = {
+    mike: 'designer',
+    jill: 'developer',
+    alicia: 'accountant'
+};
+
+let {mike, jill, alicia} = jobs;
+console.log(mike, jill, alicia);
+
+// Destructuring subsets
+let languages = ['english', 'french', 'spanish', 'german', 'japanese'];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary);
+
+// Skipping (, ,)
+
+let [, , maryNative, marySecondary] = languages;
+console.log(maryNative, marySecondary);
+
+let languages2 = {
+    firstLanguage: 'english',
+    secondLanguage: 'french',
+    thirdLanguage: 'german',
+    fourthLanguage: 'japanese'
+};
+let { firstLanguage, thirdLanguage } = languages2;
+console.log(firstLanguage, thirdLanguage);
+
+
+// Using rest parameter syntax
+
+let fruits = ['apple', 'orange', 'banana', 'peach', 'cherry'];
+let [favorite, secondFavorite, ...others] = fruits;
+console.log(favorite); 
+console.log(secondFavorite);
+console.log(others);
+
+let favoriteFoods = {
+    brian: 'pizza',
+    anna: 'pasta', 
+    sarah: 'vegetarian',
+    andrea: 'steak'
+}
+
+let {brian, anna, ...rest} = favoriteFoods;
+console.log(brian);
+console.log(anna);
+console.log(rest);
